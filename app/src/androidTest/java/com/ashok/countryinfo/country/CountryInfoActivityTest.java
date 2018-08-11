b97ec6a506d47e28dad34cfea1733b39b022b426
Ashok Kateshiya <prakash.dr@evaluationz.com>
2018-08-11 15:50:03 +0530

    Ashok Kateshiya - UI Test Implemented for title bar

---
+++ app/src/androidTest/java/com/ashok/countryinfo/country/CountryInfoActivityTest.java
@@ -0,0 +1,83 @@
+package com.ashok.countryinfo.country;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.ashok.countryinfo.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CountryInfoActivityTest {
+
+    @Rule
+    public ActivityTestRule<CountryInfoActivity> mActivityTestRule = new ActivityTestRule<>(CountryInfoActivity.class);
+
+    @Test
+    public void countryInfoActivityTest() {
+        ViewInteraction textView = onView(
+                allOf(withText("About Canada"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("About Canada")));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ViewInteraction viewGroup = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.item_list),
+                                childAtPosition(
+                                        withId(R.id.swipeRefreshLayout),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        viewGroup.check(matches(isDisplayed()));
+
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
